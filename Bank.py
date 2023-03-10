0590fc83785b2540c87700ab0a8917ef02f6d1aa
Natalia Palej <palejnatalia@gmail.com>
2023-03-10 16:50:47 +0000

    Created Bank class with TODO list

---
+++ Bank.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+from tkinter import *
+import bg as bg
+
+from Customer import Customer
+from CustomerDetails import CustomerDetails
+
+window = tk.Tk()
+window.geometry("600x600")
+window.title("Volleyball Menu")
+#bg = ImageTk.PhotoImage(Image.open("images/volleyball.jpg"))
+myBG = Label(image=bg)
+myBG.place(x=0, y=0, relwidth=1, relheight=1)
+
+window.resizable(False, False)
+
+c1 = Customer("Lilly", "Mayor", "Female", "Employed", "Athlone", "089 421 4578", "LillyMayor@gmail.com", "123456", "1234")
+c2 = Customer("Lola", "Kennedy", "Undefined", "Student", "Roscommon", "087 421 1234", "LolaKennedy@gmail.com", "987654", "9876")
+c3 = Customer("Mike", "Smith", "Male", "Unemployed", "Dublin", "086 123 4578", "MikeSmith@gmail.com", "456132", "6789")
+
+customer_list = [c1, c2, c3]
+
+
+# details screen
+def details_screen():
+    window2 = Toplevel(window, bg="grey85")
+    window2.geometry("600x500")
+    window2.title("Customer Details")
+    detail_screen = CustomerDetails(window2, customer_list)
+    detail_screen.display(0)
+
+    button = Button(window2, text="Close", width=100, height=2, command=window2.destroy, font=("arial", 10, "bold"))
+    button.pack(side=tk.BOTTOM)
+
+# exit app
+def exit_app():
+    exit()
+
+
+# customer details button
+cust_details = tk.Button(text="Customer Details", font=('Arial', 16, 'bold'), height=2, width=15, takefocus=1,
+                         command=details_screen)
+cust_details.place(x=200, y=100)
+
+
+# exit button
+exit_button = tk.Button(text="Exit", width=15, font=('Arial', 16, 'bold'), height=2, command=exit_app,
+                        bg="gray", fg="black")
+exit_button.place(x=200, y=450)
+
+window.mainloop()
