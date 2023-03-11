f4edb536ca382abfa20b94bf0414e1fd47ea9945
Natalia Palej <palejnatalia@gmail.com>
2023-03-11 11:06:59 +0000

    Created Login, Cancel and Exit buttons

--- LoginScreen.py
+++ LoginScreen.py
@@ -2,9 +2,13 @@ from tkinter import *
 import tkinter as tk
 
 
+class LoginScreen:
+    def __init__(self):
+        print("Hello")
+
+
 # TODO: Sub-Menu for CreateNewAccount
 # TODO: GUI accNo, accPIN, buttons 0-9 display
-# TODO: Login, Exit buttons
 
 # ===== Methods ===== #
 def exit_app():
@@ -17,12 +21,15 @@ window.geometry("600x600")
 window.title("ATM")
 window.resizable(False, False)
 
-login_btn = tk.Button(text="Login", font=('Arial', 16, 'bold'), height=2, width=15, takefocus=1)
-login_btn.place(x=100, y=380)
+login_btn = tk.Button(text="LOGIN", font=('Arial', 16, 'bold'), height=2, width=15, bg="limegreen")
+login_btn.place(x=80, y=380)
+
+cancel_btn = tk.Button(text="CANCEL", font=('Arial', 16, 'bold'), height=2, width=15, bg="red")
+cancel_btn.place(x=320, y=380)
 
 # exit button
-exit_btn = tk.Button(text="Exit", width=15, font=('Arial', 16, 'bold'), height=2, bg="gray", fg="black",
-                        command=exit_app)
+exit_btn = tk.Button(text="EXIT", width=15, font=('Arial', 16, 'bold'), height=2, bg="gray", fg="black",
+                     command=exit_app)
 exit_btn.place(x=200, y=500)
 
 window.mainloop()
