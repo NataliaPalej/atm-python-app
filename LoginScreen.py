b01599c56f048a4f9beaee4dd8c79eb83569b9bb
Natalia Palej <palejnatalia@gmail.com>
2023-03-11 10:59:59 +0000

    Working on LoginScreen GUI

--- LoginScreen.py
+++ LoginScreen.py
@@ -1,3 +1,28 @@
+from tkinter import *
+import tkinter as tk
+
+
 # TODO: Sub-Menu for CreateNewAccount
 # TODO: GUI accNo, accPIN, buttons 0-9 display
 # TODO: Login, Exit buttons
+
+# ===== Methods ===== #
+def exit_app():
+    exit()
+
+
+# ===== GUI ===== #
+window = tk.Tk()
+window.geometry("600x600")
+window.title("ATM")
+window.resizable(False, False)
+
+login_btn = tk.Button(text="Login", font=('Arial', 16, 'bold'), height=2, width=15, takefocus=1)
+login_btn.place(x=100, y=380)
+
+# exit button
+exit_btn = tk.Button(text="Exit", width=15, font=('Arial', 16, 'bold'), height=2, bg="gray", fg="black",
+                        command=exit_app)
+exit_btn.place(x=200, y=500)
+
+window.mainloop()
