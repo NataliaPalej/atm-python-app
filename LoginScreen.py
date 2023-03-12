d0702cade18fe3a147ce9f23e7b8de60358320b6
Natalia Palej <palejnatalia@gmail.com>
2023-03-12 00:30:53 +0000

    Added prompt to LoginScreen if PIN or AccNo is invalid

--- LoginScreen.py
+++ LoginScreen.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import Menu
 from Bank import Bank
 from NewAccount import NewAccount
+import tkinter.messagebox
 
 
 def exit_app():
@@ -15,9 +16,9 @@ def new_account():
 
 class LoginScreen:
     def __init__(self):
-        # initialize empty dictionary to store customer data from file
+        # Initialize empty dictionary to store customer data from file
         self.customers = {}
-        # read data from file
+        # Read data from file
         self.read_data()
 
         # ===== GUI ===== #
@@ -30,9 +31,9 @@ class LoginScreen:
         submenu = Menu(self.window)
         self.window.config(menu=submenu)
         new_user = Menu(submenu)
-        # name the submenu
+        # Name the submenu
         submenu.add_cascade(label="OPTIONS", menu=new_user)
-        # add option in submenu
+        # Add option in submenu
         new_user.add_command(
             label="CREATE NEW ACCOUNT", command=new_account)
 
@@ -48,7 +49,7 @@ class LoginScreen:
 
         label4 = Label(self.window, text="PIN:", fg="black", font=("arial", 20, "bold"))
         label4.place(x=80, y=180)
-        # Show *** to hide PIN input
+        # ==== Show * to hide PIN input ==== #
         self.PIN_entry = Entry(self.window, font="Arial 20", show="*")
         self.PIN_entry.insert(END, '')
         self.PIN_entry.place(x=160, y=180, width=300, height=40)
@@ -91,7 +92,7 @@ class LoginScreen:
         clearbtn = tk.Button(text="CANCEL", font=('Arial', 16, 'bold'), height=2, width=8, bg="red", command=self.clear)
         clearbtn.place(x=350, y=418)
 
-        # exit button
+        # Exit button
         exit_btn = tk.Button(text="EXIT", width=15, font=('Arial', 16, 'bold'), height=2, bg="gray", fg="black",
                              command=exit_app)
         exit_btn.place(x=15, y=520)
@@ -124,29 +125,29 @@ class LoginScreen:
         with open("customers.txt") as f:
             for line in f:
                 customer_data = line.strip().split(";")
-                # insert customer data from file to dictionary by key:value key being accNo
+                # Insert customer data from file to dictionary by key:value key being accNo
                 self.customers[customer_data[0]] = customer_data[1:]
-        # print each customer
+        # Print each customer
         for customer in self.customers:
             print(customer, self.customers[customer])
 
     def validate_login(self):
-        # read the file again to make sure new account is accessible
+        # Read the file again to make sure new account is accessible
         self.read_data()
         account = self.accNo_entry.get()
         pin = self.PIN_entry.get()
 
-        # check if account exists in dictionary
+        # Check if account exists in dictionary
         if account in self.customers:
-            # check if account and pin matches
+            # Check if account and pin matches
             if self.customers[account][0] == pin:
                 self.window.destroy()
                 Bank(self.customers, account)
                 print("Valid")
+            # Prompt the user with the error
             else:
-                print("Wrong PIN")
+                tkinter.messagebox.showerror("Login Error", "Incorrect PIN")
         else:
-            print("Account doesnt exist")
-
+            tkinter.messagebox.showerror("Login Error", "Account doesn't exist")
 
 LoginScreen().window.mainloop()
